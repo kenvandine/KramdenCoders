8f2e0d15254e51a019bfd49f5a697c1b5aef4573
Ken VanDine <ken@vandine.org>
2016-05-05 11:23:00 -0400

    imported examples

--- RaspberryPi/week/3/gpiozero_example.py
+++ RaspberryPi/week/3/gpiozero_example.py
@@ -1,14 +1,31 @@
-from gpiozero import LED, Button, Buzzer
+#!/usr/bin/python
 
+from gpiozero import LED, Button, Buzzer
+from signal import pause
+
+# Setup our LED, Button, and Buzzer
 led = LED(19)
 button = Button(6)
 buzzer = Buzzer(17)
 
-buzzer.on()
-buzzer.off()
+# Function we'll call on when_pressed event
+def pressed(arg=None):
+    led.on()
+    buzzer.on()
 
-led.on()
-led.off()
+# Function we'll call on when_released event
+def released(arg=None):
+    led.off()
+    buzzer.off()
 
-button.when_pressed = led.on
-button.when_released = led.off
+# Handle when_pressed event
+button.when_pressed = pressed
+
+# Handle when_released event
+button.when_released = released
+
+# Tell the user how to quit
+print "Press Ctrl-C to exit"
+
+# Pause and wait for butten events
+pause()
--- RaspberryPi/week/3/teleport-portal.py
+++ RaspberryPi/week/3/teleport-portal.py
@@ -1,20 +1,32 @@
+#!/usr/bin/python
+
 from mcpi.minecraft import Minecraft
 from time import sleep
 from gpiozero import LED, Buzzer, Button
 
+# Setup LED, Buzzer and Button
 led = LED(19)
 buzzer = Buzzer(17)
 button = Button(6)
 
+# Location of the portal
 x1, y1, z1 = 24, 10, -38
+
+# Location we want to teleport the player to
 x2, y2, z2 = 8, 7, -35
+
+# Teleport the player to defined location
 def teleport(foo=None):
     mc.postToChat("Teleporting!")
     mc.player.setPos(x2,y2,z2)
 
-
+# Connect to the currently running Minecraft game
 mc = Minecraft.create()
+
+# Teleport the player to a location near the portal
 teleport()
+
+# Handle the when_pressed button event
 button.when_pressed = teleport
 
 while True:
@@ -23,6 +35,7 @@ while True:
     z = round(mc.player.getPos().z)
 
     sleep(1)
+    # Check to see if the player is in the portal
     if (x, y, z) == (x1, y1, z1):
         mc.postToChat("Teleporting!")
         mc.player.setPos(x2,y2,z2)
